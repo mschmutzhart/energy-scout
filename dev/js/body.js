0da386196bb818e3c8e6f60239d8542ae0a85511
mschmutzhart <schmutzhartmichael@gmail.com>
2025-02-06 21:33:49 +0100

    version1.0

--- dev/js/body.js
+++ dev/js/body.js
@@ -4,3 +4,15 @@
 //= vendor/jquery.js
 //= lib/jquery.navkit.js
 //= lib/jquery.portfolio.js
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzUzFPuMb18DW4neylOus4nu7TpeMcIdj5VLWNZ6js6-ZCDTnBP_49MR-7MDjrgytbXrw/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+      console.log('Success!', response);
+      window.location.replace('/thank-you.html'); })
+    .catch(error => console.error('Error!', error.message))
+})
